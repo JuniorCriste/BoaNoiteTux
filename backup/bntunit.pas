2f817429a5580fe5f5121300c07fa184b564ef3f
Junior Criste <informaticode@gmail.com>
2020-09-09 11:29:57 -0300

    Base layout

---
+++ backup/bntunit.pas
@@ -0,0 +1,47 @@
+unit bntunit;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, ExtCtrls,
+  ActnList, StdCtrls, Buttons;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    BitBtn1: TBitBtn;
+    ComboBox1: TComboBox;
+    acao: TRadioGroup;
+    procedure FormCreate(Sender: TObject);
+    procedure acaoClick(Sender: TObject);
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.acaoClick(Sender: TObject);
+begin
+
+end;
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+  acao.Width:= 200;
+end;
+
+end.
+
